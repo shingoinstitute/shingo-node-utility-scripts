eb098d49fff753f20d05067c760c2cbb258d708b
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-10-27 18:30:58 -0600

    dem mappins

--- workshop-to-v2-mapper/AuthGRPCClient.interface.ts
+++ workshop-to-v2-mapper/AuthGRPCClient.interface.ts
@@ -3,9 +3,8 @@ import { IPermission } from './Permission.interface';
 import { IRole } from './Role.interface';
 
 export interface IAuthGRPCClient {
-    getUsersAsync(clause: string): Promise<IUser[]>;
-    getRolesAsync(clause: string): Promise<IRole[]>;
-    getPermissionsAsync(clause: string): Promise<IPermission[]>;
+    readUserAsync(clause: string): Promise<IUser[]>;
+    readRoleAsync(clause: string): Promise<IRole[]>;
     createPermissionAsync(permission: IPermission): Promise<any>;
     grantPermissionToUserAsync(resource: string, level: 0 | 1 | 2, userId: number): Promise<any>;
     grantPermissionToRoleAsync(resource: string, level: 0 | 1 | 2, roleId: number): Promise<any>;
